a5780afb20e6d32743abd2cf02d2f2bc032b3676
Simon Tucker <brotherlogic@gmail.com>
2025-03-03 16:11:35 -0800

    Adds CLI (#5)

---
+++ macont_cli/cli.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/brotherlogic/macont/proto"
+)
+
+func main() {
+	conn, err := grpc.Dial(os.Args[1], grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Dial fail: %v", err)
+	}
+
+	name, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("Hostname failed: %v", err)
+	}
+
+	mclient := pb.NewMacontServiceClient(conn)
+	status, err := mclient.Ping(context.Background(), &pb.PingRequest{
+		MachineName: name,
+	})
+
+	fmt.Printf("%v [%v]\n", status, err)
+}
