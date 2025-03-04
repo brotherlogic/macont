5b621991c3ce741cfed261283c509d4a679864d1
Simon <brotherlogic@gmail.com>
2025-03-03 16:23:19 -0800

    Adds request tracking for macont

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/brotherlogic/macont/proto"
 
@@ -26,6 +29,26 @@ type Server struct{}
 
 func NewServer() *Server {
 	return &Server{}
+}
+
+var (
+	serverRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "githubridge_requests",
+		Help: "The number of server requests",
+	}, []string{"method", "status"})
+)
+
+func (s *Server) ServerInterceptor(ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (interface{}, error) {
+	h, err := handler(ctx, req)
+	serverRequests.With(
+		prometheus.Labels{
+			"status": status.Convert(err).Code().String(),
+			"method": info.FullMethod}
+			).Inc()
+	return h, err
 }
 
 func main() {
